ab93a0b9c64c9f53004352bd8f92b9285915d092
Dzmitry Kishylau <dzmitry_kishylau@Dzmitrys-MacBook-Pro.local>
2022-04-27 18:11:08 -0700

    Github source: add TeamRepositories stream

--- airbyte-integrations/connectors/source-github/source_github/source.py
+++ airbyte-integrations/connectors/source-github/source_github/source.py
@@ -45,6 +45,7 @@ from .streams import (
     Tags,
     TeamMembers,
     TeamMemberships,
+    TeamRepositories,
     Teams,
     Users,
     WorkflowRuns,
@@ -225,4 +226,5 @@ class SourceGithub(AbstractSource):
             Workflows(**repository_args),
             WorkflowRuns(**repository_args),
             TeamMemberships(parent=team_members_stream, **repository_args),
+            TeamRepositories(parent=teams_stream, **organization_args),
         ]
